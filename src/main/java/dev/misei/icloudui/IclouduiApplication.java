2ebc384f01fce4100cbe4741b7d3f36e3d90ed62
David Fernández Esteban <david@misei.dev>
2025-05-25 11:30:00 +0200

    More magic

---
+++ src/main/java/dev/misei/icloudui/IclouduiApplication.java
@@ -0,0 +1,13 @@
+package dev.misei.icloudui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IclouduiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IclouduiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/misei/icloudui/IclouduiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.misei.icloudui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IclouduiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
